bdf9d1f170977f1f1f2c1bc37f7125677347bcec
Adrian Irving-Beer <wisq@wisq.net>
2026-07-24 04:27:31 -0400

    Index players by name.

--- lib/ex_mush/db/repo.ex
+++ lib/ex_mush/db/repo.ex
@@ -2,4 +2,17 @@ defmodule ExMUSH.DB.Repo do
   use Ecto.Repo,
     otp_app: :ex_mush,
     adapter: Ecto.Adapters.Postgres
+
+  alias __MODULE__
+  alias ExMUSH.DB.Object
+  import Ecto.Query, only: [from: 2]
+
+  def get_objects_for_directory do
+    from(obj in Object,
+      left_join: attr in assoc(obj, :attributes),
+      on: attr.name == "ALIAS",
+      preload: [attributes: attr]
+    )
+    |> Repo.all()
+  end
 end
--- lib/ex_mush/world/object_directory.ex
+++ lib/ex_mush/world/object_directory.ex
@@ -7,6 +7,7 @@ defmodule ExMUSH.World.ObjectDirectory do
 
   @objects_ets __MODULE__.ETS.Objects
   @contents_ets __MODULE__.ETS.Contents
+  @players_ets __MODULE__.ETS.Players
 
   def start_link(opts) do
     opts = Keyword.put_new(opts, :name, __MODULE__)
@@ -53,29 +54,71 @@ defmodule ExMUSH.World.ObjectDirectory do
 
   def contents(oid) when is_object_id(oid), do: content_oids(oid) |> Enum.map(&get/1)
 
+  def match_player(name, exact? \\ false) do
+    name = String.downcase(name)
+
+    case :ets.lookup(@players_ets, name) do
+      [{^name, oid}] -> {:ok, oid}
+      [] -> if exact?, do: {:error, :no_match}, else: partial_match_player(name)
+    end
+  end
+
+  defp partial_match_player(name) do
+    case :ets.next_lookup(@players_ets, name) do
+      {^name <> _ = match, [{match, oid}]} ->
+        # We found a partial match, but if the next ALSO matches, it's ambiguous.
+        case :ets.next(@players_ets, match) do
+          ^name <> _ -> {:error, :ambiguous_match}
+          _ -> {:ok, oid}
+        end
+
+      {_, [_]} ->
+        {:error, :no_match}
+
+      :"$end_of_table" ->
+        {:error, :no_match}
+    end
+  end
+
   @impl true
   def init(_) do
     :ets.new(@objects_ets, [:set, :protected, :named_table])
+    :ets.new(@players_ets, [:ordered_set, :protected, :named_table])
     :ets.new(@contents_ets, [:bag, :protected, :named_table])
 
     objs = load_objects()
     index_objects(objs)
+    index_players(objs)
     index_contents(objs)
 
     {:ok, nil}
   end
 
-  defp load_objects, do: DB.Repo.all(DB.Object) |> Enum.map(&World.Object.load/1)
+  defp load_objects do
+    DB.Repo.get_objects_for_directory()
+    |> Enum.map(&World.Object.load/1)
+  end
 
   defp index_objects(objs) do
     objs
-    |> Enum.map(fn o -> {o.oid.id, o} end)
+    |> Enum.map(fn obj -> {obj.oid.id, obj} end)
     |> then(&:ets.insert(@objects_ets, &1))
+  end
+
+  defp index_players(objs) do
+    objs
+    |> Enum.filter(fn obj -> obj.type == :player end)
+    |> Enum.flat_map(fn obj ->
+      [obj.name | obj.aliases]
+      |> Enum.uniq()
+      |> Enum.map(fn n -> {String.downcase(n), obj.oid} end)
+    end)
+    |> then(&:ets.insert(@players_ets, &1))
   end
 
   defp index_contents(objs) do
     objs
-    |> Enum.map(fn o -> {o.location_oid.id, o.oid} end)
+    |> Enum.map(fn obj -> {obj.location_oid.id, obj.oid} end)
     |> Enum.reject(fn {loc_id, _} -> loc_id < 0 end)
     |> then(&:ets.insert(@contents_ets, &1))
   end
