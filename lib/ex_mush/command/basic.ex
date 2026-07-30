9aed9f495a7df3ee5e4dcc4b44cc0b9d374384e2
Adrian Irving-Beer <wisq@wisq.net>
2026-07-30 02:59:57 -0400

    Placeholder look command to demo matching.

--- lib/ex_mush/command/basic.ex
+++ lib/ex_mush/command/basic.ex
@@ -1,17 +1,24 @@
 defmodule ExMUSH.Command.Basic do
   use ExMUSH.Command
   alias ExMUSH.World.Object
+  alias ExMUSH.World.Matching
 
   @command "look"
   @aliases ["l"]
   @switches ["outside"]
   @parser :one_arg
 
-  defcommand look(state, switches, target \\ "here") do
-    Object.tell(
-      state.executor,
-      inspect({state, switches, target}, label: "look", pretty: true)
-    )
+  defcommand look(state, _switches, target \\ "here") do
+    case Matching.locate(state.executor, target, %Matching.Opts{location: false}) do
+      {:ok, oid} ->
+        Object.tell(state.executor, "You look at #{Object.get(oid).name} (#{oid}).")
+
+      {:error, :no_match} ->
+        Object.tell(state.executor, "I don't see that here.")
+
+      {:error, :ambiguous_match} ->
+        Object.tell(state.executor, "I'm not sure which one you mean.")
+    end
   end
 
   @command "@teleport"
